68da3f340808277050a698d31fe5705c2c15d219
JyotiPress <printhere.in@gmail.com>
2023-12-04 22:23:09 +0530

    Done

--- 07_project/http_module.js
+++ 07_project/http_module.js
@@ -1,3 +1,11 @@
 const httpModule = require('http');
 
-httpModule.createServer
+const Server = httpModule.createServer((request, responce)=>{
+    if (request.url == '/') {
+        responce.write('<h1> I am happy to learn full stack web developement from PW Skills!</h1>');        
+    }
+    responce.end();
+});
+Server.listen(5000);
+
+console.log("http module is running on port 5000");
